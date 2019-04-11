0cec4ecafdcf32b282fd0dd37918104d4421413a
bmanga <bruno.manga95@gmail.com>
2019-04-11 16:34:10 +0100

    tools: revert fmt requirement, because it does not play well with clang CI

--- tools/IMUStreamer/main.cpp
+++ tools/IMUStreamer/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <thread>
 #include "IMU/IMU.h"
-#include "fmt/format.h"
 #include "telemetry/server.h"
 
 struct sensors_data {
@@ -21,7 +20,7 @@ struct packet {
 int main()
 {
   tel::server server;
-  server.run_on_thread(9004);
+  server.run_on_thread(9006);
 
   Sensor_IMU imu_1(0);
   Sensor_IMU imu_2(2);
@@ -56,12 +55,6 @@ int main()
     data.c.gy = imu_3.getGyro_Y() / 16384.0;
     data.c.gz = imu_3.getGyro_Z() / 16384.0;
 
-    std::cout << fmt::format(
-                     "A: {: f} {: f} {: f} -- B: {: f} {: f} {: f} -- C: {: f} "
-                     "{: f} {: f}",
-                     data.a.ax, data.a.ay, data.a.az, data.b.ax, data.b.ay,
-                     data.b.az, data.c.ax, data.c.ay, data.c.az)
-              << std::endl;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     server.broadcast(&data, sizeof(data));
