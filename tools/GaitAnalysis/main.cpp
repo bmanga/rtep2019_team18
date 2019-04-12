86912f7e4b8a146523c10f0b050b0a7c7d1c0f67
bmanga <bruno.manga95@gmail.com>
2019-04-12 10:52:21 +0100

    tools: set up basics of the gait-analysis tool

--- tools/GaitAnalysis/main.cpp
+++ tools/GaitAnalysis/main.cpp
@@ -1,4 +1,19 @@
-int main()
+#include <iostream>
+#include "telemetry/client.h"
+
+void on_message(const void *d, long len) {}
+
+int main(int argc, char *argv[])
 {
-  return 0;
+  if (argc != 2) {
+    std::cout << "Invalid number of arguments: Requires  only the "
+                 "address of the streamer server to connect to.";
+    return -1;
+  }
+
+  tel::client client;
+  client.set_message_handler(&on_message);
+
+  client.connect_to(argv[1]);
+  client.run_on_thread();
 }
