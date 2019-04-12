d561d0da1f18ad033ac9dcf32847d0d8006c4a28
conormcharg <conormcharg@live.co.uk>
2019-04-12 16:27:36 +0100

    clang formatted mp3 lib

--- lib/mp3/mp3.cpp
+++ lib/mp3/mp3.cpp
@@ -7,50 +7,44 @@
 
 mp3_player::mp3_player()
 {
-	
-    ao_initialize();
-    driver = ao_default_driver_id();
-    mpg123_init();
-    mh = mpg123_new(NULL, &err);
-    buffer_size = mpg123_outblock(mh);
-    buffer = (char*) malloc(buffer_size * sizeof(char));
-    count = 0;
-
+  ao_initialize();
+  driver = ao_default_driver_id();
+  mpg123_init();
+  mh = mpg123_new(NULL, &err);
+  buffer_size = mpg123_outblock(mh);
+  buffer = (char *)malloc(buffer_size * sizeof(char));
+  count = 0;
 }
 
 mp3_player::~mp3_player()
 {
-    free(buffer);
-    ao_close(dev);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    mpg123_exit();
-    ao_shutdown();
+  free(buffer);
+  ao_close(dev);
+  mpg123_close(mh);
+  mpg123_delete(mh);
+  mpg123_exit();
+  ao_shutdown();
 }
 
-int mp3_player::play(char* filename)
+int mp3_player::play(char *filename)
 {
-	
-    /* open the file and get the decoding format */
-    
-    mpg123_open(mh, filename);
-    mpg123_getformat(mh, &rate, &channels, &encoding);
+  /* open the file and get the decoding format */
 
-    /* set the output format and open the output device */
-    format.bits = mpg123_encsize(encoding) * BITS;
-    format.rate = rate;
-    format.channels = channels;
-    format.byte_format = AO_FMT_NATIVE;
-    format.matrix = 0;
-    dev = ao_open_live(driver, &format, NULL);
+  mpg123_open(mh, filename);
+  mpg123_getformat(mh, &rate, &channels, &encoding);
 
-    /* decode and play */
-    while (mpg123_read(mh, (unsigned char*) buffer, buffer_size, &done) == MPG123_OK)
-        ao_play(dev, buffer, done);
+  /* set the output format and open the output device */
+  format.bits = mpg123_encsize(encoding) * BITS;
+  format.rate = rate;
+  format.channels = channels;
+  format.byte_format = AO_FMT_NATIVE;
+  format.matrix = 0;
+  dev = ao_open_live(driver, &format, NULL);
 
-  
-    
-    return 0;
-    
+  /* decode and play */
+  while (mpg123_read(mh, (unsigned char *)buffer, buffer_size, &done) ==
+         MPG123_OK)
+    ao_play(dev, buffer, done);
+
+  return 0;
 }
-
